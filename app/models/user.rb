d8cdecc7dd7f91aaefeb816313e9c5f8165d65cb
Scott Firestone <scott.firestone.dev@gmail.com>
2016-04-05 17:27:41 -0600

    Fix account base show
    Avatar properly appears
    Spotify link points to proper destination

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
       new_user.name          = auth_info[:info][:name]
       new_user.nickname      = auth_info[:info][:nickname]
       new_user.email         = auth_info[:info][:email]
-      new_user.spotify       = auth_info[:info][:spotify]
+      new_user.spotify       = auth_info[:info][:urls][:spotify]
       new_user.image         = auth_info[:info][:image]
       new_user.token         = auth_info[:credentials][:token]
       new_user.refresh_token = auth_info[:credentials][:refresh_token]
